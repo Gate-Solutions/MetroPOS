9b70928cf22ae47561cd183ba2458b7359c17d3d
Muhammad Rehan Shafqat <rehanshafqat2004@gmail.com>
2024-12-01 16:29:03 +0500

    database setup

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module org.example.metropos {
     requires javafx.controls;
     requires javafx.fxml;
+    requires static lombok;
+    requires com.zaxxer.hikari;
+    requires org.jooq;
 
 
-    opens org.example.metropos to javafx.fxml;
-    exports org.example.metropos;
+    opens org.gate.metropos to javafx.fxml;
+    exports org.gate.metropos;
 }
---
+++ src/main/java/org/gate/metropos/config/DatabaseConfig.java
@@ -0,0 +1,50 @@
+package org.gate.metropos.config;
+
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+
+public class DatabaseConfig {
+    private static HikariDataSource localDataSource;
+    private static HikariDataSource cloudDataSource;
+
+    public static DSLContext getLocalContext() {
+        if (localDataSource == null) {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl("jdbc:mysql://localhost:3306/local_db");
+            config.setUsername("root");
+            config.setPassword("");
+            config.setMaximumPoolSize(10);
+
+            localDataSource = new HikariDataSource(config);
+        }
+        return DSL.using(localDataSource, SQLDialect.MYSQL);
+    }
+
+    public static DSLContext getCloudContext() {
+        if (cloudDataSource == null) {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl("jdbc:mysql://localhost:3306/cloud_db");
+            config.setUsername("root");
+            config.setPassword("");
+            config.setMaximumPoolSize(10);
+
+            cloudDataSource = new HikariDataSource(config);
+        }
+        return DSL.using(cloudDataSource, SQLDialect.MYSQL);
+    }
+
+    public static void closeDataSources() {
+        if (localDataSource != null) {
+            System.out.println("Closing local");
+            localDataSource.close();
+        }
+        if (cloudDataSource != null) {
+            System.out.println("Closing cloud");
+            cloudDataSource.close();
+        }
+    }
+}
