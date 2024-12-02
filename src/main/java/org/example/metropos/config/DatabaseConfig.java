a22f6d08f2937ac5c6f7c14a1d105a73146837bd
Asad Shafiq <masadali5433@gmail.com>
2024-12-02 17:01:31 +0500

    add database intialization classes

--- src/main/java/org/example/metropos/config/DatabaseConfig.java
+++ src/main/java/org/example/metropos/config/DatabaseConfig.java
@@ -2,40 +2,64 @@ package org.example.metropos.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
 
 public class DatabaseConfig {
     private static HikariDataSource localDataSource;
     private static HikariDataSource remoteDataSource;
+    private static DSLContext localDsl;
+    private static DSLContext remoteDsl;
 
     private static void initializeRemoteDataSources() {
-        HikariConfig remoteConfig = new HikariConfig();
-        remoteConfig.setJdbcUrl("jdbc:postgresql://remote-host:5432/remote_db");
-        remoteConfig.setUsername("remote_user");
-        remoteConfig.setPassword("remote_password");
-        remoteDataSource = new HikariDataSource(remoteConfig);
+        try {
+            HikariConfig remoteConfig = new HikariConfig();
+            remoteConfig.setJdbcUrl("jdbc:postgresql://remote-host:5432/remote_db");
+            remoteConfig.setUsername("postgres");
+            remoteConfig.setPassword("asad123");
+            remoteDataSource = new HikariDataSource(remoteConfig);
+            remoteDsl = DSL.using(remoteDataSource, SQLDialect.POSTGRES);
+        } catch (Exception e) {
+            System.out.println("Couldn't connect to remote database");
+        }
 
     }
 
     private static void initializeLocalDataSource() {
         HikariConfig localConfig = new HikariConfig();
         localConfig.setJdbcUrl("jdbc:postgresql://localhost:5432/local_db");
-        localConfig.setUsername("local_user");
-        localConfig.setPassword("local_password");
+        localConfig.setUsername("postgres");
+        localConfig.setPassword("asad123");
         localDataSource = new HikariDataSource(localConfig);
+        localDsl = DSL.using(localDataSource, SQLDialect.POSTGRES);
     }
 
-    public static HikariDataSource getLocalContext() {
-        if(localDataSource == null) {
+    public static DSLContext getLocalDSL() {
+        if(localDsl == null) {
             initializeLocalDataSource();
         }
-        return localDataSource;
+        return localDsl;
     }
 
-    public static HikariDataSource getRemoteContext() {
-        if(remoteDataSource == null) {
+    public static DSLContext getRemoteDSL() {
+        if(remoteDsl == null) {
             initializeRemoteDataSources();
         }
-        return remoteDataSource;
+        return remoteDsl;
+    }
+
+    public boolean isRemoteAvailable() {
+        return remoteDsl != null;
+    }
+
+    public void shutdown() {
+        if (localDataSource != null) {
+            localDataSource.close();
+        }
+        if (remoteDataSource != null) {
+            remoteDataSource.close();
+        }
     }
 
 }
---
+++ src/main/java/org/example/metropos/database/DatabaseInitializer.java
@@ -0,0 +1,22 @@
+package org.example.metropos.database;
+
+import org.jooq.DSLContext;
+
+
+public class DatabaseInitializer {
+    private final DSLContext dsl;
+
+    public DatabaseInitializer(DSLContext dsl) {
+        this.dsl = dsl;
+    }
+
+    public void createTables() {
+//        Add All the tables
+
+    }
+
+//    Add functions to create tables
+
+
+
+}
