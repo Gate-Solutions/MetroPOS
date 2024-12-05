011d247bd1c2da1ca79dcd5dbfac239b0fc29c99
Asad Shafiq <Masadali5433@gmail.com>
2024-12-06 01:22:09 +0500

    add employee services

---
+++ src/main/java/org/gate/metropos/enums/EmployeeFields.java
@@ -0,0 +1,29 @@
+package org.gate.metropos.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.jooq.Field;
+import org.jooq.Table;
+import org.jooq.impl.DSL;
+
+@Getter
+@AllArgsConstructor
+public enum EmployeeFields {
+    EmployeeTable("employees"),
+    NAME("name"),
+    EMPLOYEE_NO("employee_no"),
+    IS_ACTIVE("is_active"),
+    IS_FIRST_TIME("is_first_time"),
+    SALARY("salary"),
+    BRANCH_ID("branch_id");
+
+    private final String columnName;
+
+    public Field<Object> toField() {
+        return DSL.field(columnName);
+    }
+
+    public Table<?> toTableField() {
+        return DSL.table(EmployeeTable.getColumnName());
+    }
+}
